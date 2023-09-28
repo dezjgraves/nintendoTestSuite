5805b00285447ea276dc64a3bb103583d504513e
Desmond Graves <desmondjasongraves@gmail.com>
2023-09-27 20:37:35 -0600

    Added a ton of stuff. Started adding expects

--- nintendoTestSuite/navbarPageObject.ts
+++ nintendoTestSuite/navbarPageObject.ts
@@ -1,51 +1,64 @@
-import {By} from 'selenium-webdriver';
+import {By, until} from 'selenium-webdriver';
 import {BasePage} from './basePage';
 
 export class nintendoWeb extends BasePage {
-    //Home
-    homeBtn: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR"])[1]');
-    //Search Bar
-    searchBar: By = By.xpath('//button[@class="SearchInputstyles__Button-sc-1r59ztq-3 jYcoFG"]');
-    searchDrop: By = By.xpath('(//div[@class="SearchInputstyles__Select-sc-1r59ztq-7 bsKuBq css-b62m3t-container"])[1]');
-    searchClose: By = By.xpath('(//button[@class="SearchFlyoverstyles__Close-sc-14rqey7-6 bMRZPH"])[1]');
-    //Top Links
-    supportBtn: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR Topstyles__TopLink-sc-14wlync-2 gXUCck"])[1]');
-    wishBtn: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR Topstyles__TopLink-sc-14wlync-2 gXUCck"])[2]');
-    cartBtn: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR Topstyles__TopLink-sc-14wlync-2 gXUCck"])[2]');
-    loginBtn: By = By.xpath('//button[@class="ButtonLink-sc-1empns-0 bswXBR UserButtonstyles__Button-sc-jxbhoc-0 goYvYP"]');
-    loginClose: By = By.xpath('//button[@class="CloseButtonstyles__CloseButton-sc-auhxh6-0 eEsMrv DrawerHeaderstyles__CloseButton-sc-q0gw4c-2 jPtbyp"]');
+        //Home
+        homeBtn: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR"])[1]');
+        //Search Bar
+        searchBar: By = By.xpath('//button[@class="SearchInputstyles__Button-sc-1r59ztq-3 jYcoFG"]');
+        searchDrop: By = By.xpath('(//div[@class="SearchInputstyles__Select-sc-1r59ztq-7 bsKuBq css-b62m3t-container"])[1]');
+        searchClose: By = By.xpath('(//button[@class="SearchFlyoverstyles__Close-sc-14rqey7-6 bMRZPH"])[1]');
+        //Top Links
+        supportBtn: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR Topstyles__TopLink-sc-14wlync-2 gXUCck"])[1]');
+        wishBtn: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR Topstyles__TopLink-sc-14wlync-2 gXUCck"])[2]');
+        cartBtn: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR Topstyles__TopLink-sc-14wlync-2 gXUCck"])[2]');
+        loginBtn: By = By.xpath('//button[@class="ButtonLink-sc-1empns-0 bswXBR UserButtonstyles__Button-sc-jxbhoc-0 goYvYP"]');
+        loginClose: By = By.xpath('//button[@class="CloseButtonstyles__CloseButton-sc-auhxh6-0 eEsMrv DrawerHeaderstyles__CloseButton-sc-q0gw4c-2 jPtbyp"]');
 
-    //Dropdown Links
-    storeDrop: By = By.xpath('(//button[@class="ButtonLink-sc-1empns-0 bswXBR DropdownButtonstyles__Button-sc-m78d24-0 jZacJP"]) [1]');
-    gamesDrop: By = By.xpath('(//button[@class="ButtonLink-sc-1empns-0 bswXBR DropdownButtonstyles__Button-sc-m78d24-0 jZacJP"]) [2]');
-    switchDrop: By = By.xpath('(//button[@class="ButtonLink-sc-1empns-0 bswXBR DropdownButtonstyles__Button-sc-m78d24-0 jZacJP"]) [3]');
-    playDrop: By = By.xpath('(//button[@class="ButtonLink-sc-1empns-0 bswXBR DropdownButtonstyles__Button-sc-m78d24-0 jZacJP"]) [4]');
-    closeDrop: By = By.xpath('//button[@class="CloseButtonstyles__CloseButton-sc-auhxh6-0 eEsMrv Awningstyles__AwningClose-sc-12iyqmw-1 fwKpOS"]')
-    newsDrop: By = By.xpath('//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownButtonstyles__Button-sc-m78d24-0 jZacJP"]')
-    closeDrop2: By = By.xpath('//button[@class="CloseButtonstyles__CloseButton-sc-auhxh6-0 eEsMrv"]');
-    closeDrop3: By = By.xpath('//button[@class="CloseButtonstyles__CloseButton-sc-auhxh6-0 eEsMrv"]');
+        //Dropdown Links
+        storeDrop: By = By.xpath('(//button[@class="ButtonLink-sc-1empns-0 bswXBR DropdownButtonstyles__Button-sc-m78d24-0 jZacJP"]) [1]');
+        gamesDrop: By = By.xpath('(//button[@class="ButtonLink-sc-1empns-0 bswXBR DropdownButtonstyles__Button-sc-m78d24-0 jZacJP"]) [2]');
+        switchDrop: By = By.xpath('(//button[@class="ButtonLink-sc-1empns-0 bswXBR DropdownButtonstyles__Button-sc-m78d24-0 jZacJP"]) [3]');
+        playDrop: By = By.xpath('(//button[@class="ButtonLink-sc-1empns-0 bswXBR DropdownButtonstyles__Button-sc-m78d24-0 jZacJP"]) [4]');
+        closeDrop: By = By.xpath('//button[@class="CloseButtonstyles__CloseButton-sc-auhxh6-0 eEsMrv Awningstyles__AwningClose-sc-12iyqmw-1 fwKpOS"]');
+        newsDrop: By = By.xpath('//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownButtonstyles__Button-sc-m78d24-0 jZacJP"]');
+        closeDrop2: By = By.xpath('//button[@class="CloseButtonstyles__CloseButton-sc-auhxh6-0 eEsMrv"]');
+        closeDrop3: By = By.xpath('//button[@class="CloseButtonstyles__CloseButton-sc-auhxh6-0 eEsMrv"]');
 
-    //dropdown SubCategories
-    subGames: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__HeadingLink-sc-fwuyy1-11 fmrMnO"])[1]')
-    subHard: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__HeadingLink-sc-fwuyy1-11 fmrMnO"])[2]')
-    subMerch: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__HeadingLink-sc-fwuyy1-11 fmrMnO"])[3]')
-    subExclusives: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__HeadingLink-sc-fwuyy1-11 fmrMnO"])[4]')
-    subCharacters: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__HeadingLink-sc-fwuyy1-11 fmrMnO"])[5]')
-    subDeals: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__HeadingLink-sc-fwuyy1-11 fmrMnO"])[6]')    
+        //dropdown SubCategories
+        subGames: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__HeadingLink-sc-fwuyy1-11 fmrMnO"])[1]');
+        subHard: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__HeadingLink-sc-fwuyy1-11 fmrMnO"])[2]');
+        subMerch: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__HeadingLink-sc-fwuyy1-11 fmrMnO"])[3]');
+        subExclusives: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__HeadingLink-sc-fwuyy1-11 fmrMnO"])[4]');
+        subCharacters: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__HeadingLink-sc-fwuyy1-11 fmrMnO"])[5]');
+        subDeals: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__HeadingLink-sc-fwuyy1-11 fmrMnO"])[6]') ;   
+        
+        //dropdown StoreLinks
+        storeLink1: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[1]');
+        storeLink2: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[2]');
+        storeLink3: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[3]');
+        storeLink4: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[4]');
+        storeLink5: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[5]');
+        storeLink6: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[6]');
+        storeLink7: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[7]');
+        storeLink8: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[8]');
+        storeLink9: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[9]');
+        storeLink10: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[10]');
+        storeLink11: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[11]');
+        storeLink12: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[12]');
+        storeLink13: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[13]');
+        storeLink14: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[14]');
+        storeLink15: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[15]');
+        storeLink16: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[16]');
+        storeLink17: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[17]');
+        storeLink18: By = By.xpath('(//a[@class="ButtonLink-sc-1empns-0 bswXBR DropdownMenustyles__LinkItem-sc-fwuyy1-4 bWVJoL"])[18]');
+
+    
     constructor () {
-        super ({url: "https://www.nintendo.com/error/"})
+        super ({url: "https://www.nintendo.com/switch/online/"})
     };
 
-    /////////////async functions here
-
-//     async tabSwitch(){
-//         let myTabs = await this.driver.getAllWindowHandles();
-//         await this.driver.switchTo().window(myTabs[1]);
-//         await this.driver.sleep(1500);
-//         await this.driver.close();
-//         await this.driver.switchTo().window(myTabs [0]);
-//     }
-// }
+    /////////////Methods here
 
 async tabSwitch() {
     const myTabs = await this.driver.getAllWindowHandles();
@@ -85,4 +98,22 @@ async tabSwitch2() {
     }
 }
 
+async openPage(){
+    await this.navigate();
+    await this.driver.manage().window().maximize();
 }
+
+async homeButtonClick(){
+    
+    await this.driver.wait(until.elementIsVisible(this.driver.findElement(this.homeBtn)))
+    await this.driver.findElement(this.homeBtn);
+    await this.click(this.homeBtn);
+    let homeUrl = await this.driver.getCurrentUrl();
+    return homeUrl;
+    const urlAfterClick = await this.homeButtonClick();
+    expect(urlAfterClick).toContain("https://www.nintendo.com/");
+    await urlAfterClick, (e) => {
+    if (e) console.log(e);
+    else console.log('Successfully clicked homepage');
+    };
+}};
--- nintendoTestSuite/navbarTest.test.ts
+++ nintendoTestSuite/navbarTest.test.ts
@@ -1,58 +1,251 @@
 import { nintendoWeb } from "./navbarPageObject";
 const nintendo = new nintendoWeb();
+const fs = require('fs');
 
 describe("Testing all functions of the Navbar", () => {
-
-    test ('Open page and click home button', async () => {
-            
-            await nintendo.navigate();
-            await nintendo.driver.manage().window().maximize();
-            await nintendo.driver.findElement(nintendo.homeBtn);
-            await nintendo.click(nintendo.homeBtn);
-            await nintendo.driver.findElement(nintendo.searchBar);
-            await nintendo.click(nintendo.searchBar);
-            await nintendo.driver.findElement(nintendo.searchClose);
-            await nintendo.click(nintendo.searchClose);
-            await nintendo.driver.findElement(nintendo.searchDrop);
-            await nintendo.click(nintendo.searchDrop);
-            await nintendo.driver.findElement(nintendo.supportBtn);
-            await nintendo.click(nintendo.supportBtn);
-            await nintendo.tabSwitch();
-            await nintendo.driver.sleep(1500);
-            await nintendo.driver.findElement(nintendo.wishBtn);
-            await nintendo.click(nintendo.wishBtn);
-            await nintendo.driver.findElement(nintendo.cartBtn);
-            await nintendo.click(nintendo.cartBtn);
-            await nintendo.driver.sleep(1500);
-            await nintendo.tabSwitch2();
-            await nintendo.driver.findElement(nintendo.loginBtn);
-            await nintendo.click(nintendo.loginBtn);
-            await nintendo.driver.findElement(nintendo.loginClose);
-            await nintendo.click(nintendo.loginClose);
-            await nintendo.driver.findElement(nintendo.storeDrop);
-            await nintendo.click(nintendo.storeDrop);
-            // await nintendo.driver.sleep(1500);
-            await nintendo.click(nintendo.closeDrop);
-            await nintendo.driver.findElement(nintendo.gamesDrop);
-            await nintendo.click(nintendo.gamesDrop);
-            // await nintendo.driver.sleep(1500);
-            await nintendo.click(nintendo.closeDrop2);
-            await nintendo.driver.findElement(nintendo.switchDrop);
-            await nintendo.click(nintendo.switchDrop);
-            // await nintendo.driver.sleep(1500);
-            await nintendo.click(nintendo.closeDrop2);
-            await nintendo.driver.findElement(nintendo.newsDrop);
-            await nintendo.click(nintendo.newsDrop);
-            await nintendo.driver.findElement(nintendo.playDrop);
-            await nintendo.click(nintendo.playDrop);
-            // await nintendo.driver.sleep(1500);
-            await nintendo.click(nintendo.closeDrop2);
-            //My Nintendo Store Sub Menu
-            await nintendo.driver.findElement(nintendo.storeDrop);
-            await nintendo.click(nintendo.storeDrop)
-            await nintendo.driver.findElement(nintendo.subGames);
-            await nintendo.click(nintendo.subGames);
-  
-
+    beforeAll(() => {
+        nintendo.openPage();
     });
-});
+    afterAll(() => {
+        nintendo.driver.quit();
+    })
+    test ('Click Homepage', async () =>{
+        nintendo.homeButtonClick
+         //maximize window
+         await nintendo.navigate();
+         await nintendo.driver.manage().window().maximize();
+         await nintendo.driver.findElement(nintendo.homeBtn);
+         await nintendo.click(nintendo.homeBtn);
+
+         await nintendo.driver.findElement(nintendo.searchBar);
+         await nintendo.click(nintendo.searchBar);
+         await nintendo.driver.findElement(nintendo.searchClose);
+         await nintendo.click(nintendo.searchClose);
+
+         await nintendo.driver.findElement(nintendo.searchDrop);
+         await nintendo.click(nintendo.searchDrop);
+
+         await nintendo.driver.findElement(nintendo.supportBtn);
+         await nintendo.click(nintendo.supportBtn);
+         await nintendo.tabSwitch();
+         await nintendo.driver.sleep(1500);
+
+         await nintendo.driver.findElement(nintendo.wishBtn);
+         await nintendo.click(nintendo.wishBtn);
+
+         await nintendo.driver.findElement(nintendo.cartBtn);
+         await nintendo.click(nintendo.cartBtn);
+         await nintendo.driver.sleep(1500);
+         await nintendo.tabSwitch2();
+
+         await nintendo.driver.findElement(nintendo.loginBtn);
+         await nintendo.click(nintendo.loginBtn);
+
+         await nintendo.driver.findElement(nintendo.loginClose);
+         await nintendo.click(nintendo.loginClose);
+
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop);
+         // await nintendo.driver.sleep(1500);
+         await nintendo.click(nintendo.closeDrop);
+
+         await nintendo.driver.findElement(nintendo.gamesDrop);
+         await nintendo.click(nintendo.gamesDrop);
+         // await nintendo.driver.sleep(1500);
+         await nintendo.click(nintendo.closeDrop2);
+
+         await nintendo.driver.findElement(nintendo.switchDrop);
+         await nintendo.click(nintendo.switchDrop);
+         // await nintendo.driver.sleep(1500);
+         await nintendo.click(nintendo.closeDrop2);
+
+         await nintendo.driver.findElement(nintendo.newsDrop);
+         await nintendo.click(nintendo.newsDrop);
+
+         await nintendo.driver.findElement(nintendo.playDrop);
+         await nintendo.click(nintendo.playDrop);
+         // await nintendo.driver.sleep(1500);
+         await nintendo.click(nintendo.closeDrop2);
+
+         //My Nintendo Store Sub Menu
+
+                     //Store games
+
+         await nintendo.navigate();
+         await nintendo.driver.sleep(500);
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop);
+         await nintendo.driver.findElement(nintendo.subGames);
+         await nintendo.click(nintendo.subGames);
+
+                     //Store Hardware
+
+         await nintendo.navigate();
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop);
+         await nintendo.driver.findElement(nintendo.subHard);
+         await nintendo.click(nintendo.subHard);
+
+                     //Store Merch
+
+         await nintendo.navigate();
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop);
+         await nintendo.driver.findElement(nintendo.subMerch);
+         await nintendo.click(nintendo.subMerch);
+
+                     //Store Exclusives
+         await nintendo.navigate();
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop);
+         await nintendo.driver.findElement(nintendo.subExclusives);
+         await nintendo.click(nintendo.subExclusives);
+
+                     //Store Characters
+         await nintendo.navigate();
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.subCharacters);
+         await nintendo.click(nintendo.subCharacters);
+
+                     //Store Sales and Deals
+
+         await nintendo.navigate();
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.subDeals);
+         await nintendo.click(nintendo.subDeals);
+
+//Store Sub Links (18)
+
+
+         await nintendo.navigate();
+         await nintendo.driver.sleep(500);
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink1);
+         await nintendo.click(nintendo.storeLink1);
+
+         await nintendo.navigate();
+         await nintendo.driver.sleep(500);
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink2);
+         await nintendo.click(nintendo.storeLink2);
+
+         await nintendo.navigate();
+         await nintendo.driver.sleep(500);
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink3);
+         await nintendo.click(nintendo.storeLink3);
+
+         await nintendo.navigate();
+         await nintendo.driver.sleep(500);
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink4);
+         await nintendo.click(nintendo.storeLink4);
+
+         await nintendo.navigate();
+         await nintendo.driver.sleep(500);
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink5);
+         await nintendo.click(nintendo.storeLink5);
+
+         await nintendo.navigate();
+         await nintendo.driver.sleep(500);
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink6);
+         await nintendo.click(nintendo.storeLink6);
+
+         await nintendo.navigate();
+         await nintendo.driver.sleep(500);
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink7);
+         await nintendo.click(nintendo.storeLink7);
+
+         await nintendo.navigate();
+         await nintendo.driver.sleep(500);
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink8);
+         await nintendo.click(nintendo.storeLink8);
+
+         await nintendo.navigate();
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink9);
+         await nintendo.click(nintendo.storeLink9);
+
+         await nintendo.navigate();
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink10);
+         await nintendo.click(nintendo.storeLink10);
+
+         await nintendo.navigate();
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink11);
+         await nintendo.click(nintendo.storeLink11);
+
+         await nintendo.navigate();
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink12);
+         await nintendo.click(nintendo.storeLink12);
+
+         await nintendo.navigate();
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink13);
+         await nintendo.click(nintendo.storeLink13);
+
+         await nintendo.navigate();
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink14);
+         await nintendo.click(nintendo.storeLink13);
+
+         await nintendo.navigate();
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink14);
+         await nintendo.click(nintendo.storeLink12);
+
+         await nintendo.navigate();
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink15);
+         await nintendo.click(nintendo.storeLink15);
+
+         await nintendo.navigate();
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink16);
+         await nintendo.click(nintendo.storeLink16);
+
+         await nintendo.navigate();
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink17);
+         await nintendo.click(nintendo.storeLink17);
+
+         await nintendo.navigate();
+         await nintendo.driver.findElement(nintendo.storeDrop);
+         await nintendo.click(nintendo.storeDrop)
+         await nintendo.driver.findElement(nintendo.storeLink18);
+         await nintendo.click(nintendo.storeLink18);
+
+ });
+    })
+
+    
+
+
+           
+    
